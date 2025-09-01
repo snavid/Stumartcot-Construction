e9dec0132ac2d69399c9aec8251da63015bbf87d
YUNUS <snavidux.official@gmail.com>
2025-09-01 15:34:43 +0300

    updated main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def main():
-    print("Hello from stumartcot-construction!")
+from flask import Flask
+from website import create_app
 
+app = create_app()
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    app.run(debug=True)
