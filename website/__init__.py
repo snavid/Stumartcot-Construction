bd8ebe8cfe81fc6a3b2d675ce5d6974806ac622c
YUNUS <snavidux.official@gmail.com>
2025-10-18 14:41:43 +0300

    updated stemap

--- website/__init__.py
+++ website/__init__.py
@@ -10,7 +10,7 @@ DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__, static_folder='static')
-    #app.config["SERVER_NAME"] = "stumarcot.co.tz"
+    app.config["SERVER_NAME"] = "stumarcot.co.tz"
     #app.config["PREFERRED_URL_SCHEME"] = "https"
     app.config['SECRET_KEY'] = 'your-secret-key-here'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
